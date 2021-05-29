2f9c184046f5eca9bc9b4dd5b22934b819240ec2
潘越 <panyue@panyuedeMacBook-Pro.local>
2021-05-29 16:10:13 +0800

    react-router基础学习

---
+++ spa_router/src/pages/About/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class About extends Component {
+    render() {
+        // console.log('About组件收到是的props是:',this.props);
+        return (
+            <div>
+                <h3>我是About的内容</h3>
+            </div>
+        );
+    }
+}
+
+export default About;
---
+++ spa_router/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h3>我是Home的内容</h3>
+            </div>
+        );
+    }
+}
+
+export default Home;
