650639a1752aed624d0cf790acec173f84a7084a
潘越 <panyue@panyuedeMacBook-Pro.local>
2021-05-29 14:31:38 +0800

    删除一些文件

--- demo1/src/components/welcome/Welcome.js
+++
@@ -1,9 +0,0 @@
-import React,{Component} from "react";
-import './welcome.css';
-export  default  class Welcome extends Component{
-    render() {
-        return(
-           <h1 className="info">welcome to react</h1>
-        )
-    }
-}
